dc6fc7a9c48303a22ad78ae340f6db57ec32fc8e
David Barnes <david.barnes@relativity.com>
2022-09-08 12:47:06 -0500

    start of a basic front end

---
+++ src/MemoryOverflow/ClientApp/src/app/posts/posts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  posts$ = of([{ title: "hello" }, { title: "world" }]);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
